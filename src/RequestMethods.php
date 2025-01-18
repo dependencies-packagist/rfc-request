81e167deae47f9028c9f2e77671798c664d474be
jundayw <jundayw@126.com>
2025-01-18 20:51:02 +0800

    RequestMethod

---
+++ src/RequestMethods.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Rfc\Request;
+
+interface RequestMethods extends RequestMethod
+{
+    public const HEAD = ['HEAD'];
+    public const GET = ['GET'];
+    public const POST = ['POST'];
+    public const PUT = ['PUT'];
+    public const PATCH = ['PATCH'];
+    public const DELETE = ['DELETE'];
+    public const PURGE = ['PURGE'];
+    public const OPTIONS = ['OPTIONS'];
+    public const TRACE = ['TRACE'];
+    public const CONNECT = ['CONNECT'];
+
+}
